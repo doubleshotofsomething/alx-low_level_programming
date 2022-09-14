aa5aac1c22b59d21afa12cfa0363c102b6b7fba8
Solo <solofelangmaseko@gmail.com>
2022-09-13 20:22:28 -0700

    loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _abs - function comoutes absoluter value of integer
+ * @ab: integer input
+ *
+ * return: absolute value of integer
+ */
+int _abs(int ab)
+{
+	return (ab * ((ab > 0) - (ab < 0)));
+}	
