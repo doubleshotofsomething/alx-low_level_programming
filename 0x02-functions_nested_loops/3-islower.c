242ee71fa3124c42938290aa44f732cd513d7d1b
Solo <solofelangmaseko@gmail.com>
2022-09-13 19:38:52 -0700

    loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - function that checks for lowercase character
+ *
+ * @c: single letter input
+ *
+ * return: 1 if c is lowercase, and 0 if not
+ */
+int _islower(int c)
+{
+	if (c => 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
