d2ebf735a6efc0d66eb6914e188743c99e73f069
Solo <solofelangmaseko@gmail.com>
2022-09-22 10:11:03 -0700

    Pointers

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string.
+ * @n: bytes.
+ * @src: output string.
+ * @dest: intput.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int m;
+
+	for (m = 0; m < n && src[m] != '\0'; m++)
+		*(dest + m) = *(src + m);
+	for ( ; m < n; m++)
+		*(dest + m) = '\0';
+	return (dest);
+}
