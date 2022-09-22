65384f1b8c56a8e06a1b77694ee7edca91ac37e4
Solo <solofelangmaseko@gmail.com>
2022-09-22 10:53:04 -0700

    Pointers

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters to uppercase, in a string.
+ * @l: string.
+ * Return: results.
+ */
+
+char *string_toupper(char *)
+{
+	int n = 0;
+
+	while (*(l + n) != '\0')
+	{
+		if (*(l + n) >= 'a' && *(l + n) <= 'z')
+		{
+			*(l + n) = *(l + n) - 32;
+		}
+		n++;
+	}
+	return (n);
+}
