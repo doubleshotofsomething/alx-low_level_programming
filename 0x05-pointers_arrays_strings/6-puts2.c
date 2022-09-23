bc35bde80c3a45fd32cdf27885abf968ebf6d606
Solo <solofelangmaseko@gmail.com>
2022-09-23 00:47:56 -0700

    Pointers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - print parts of string.
+ * @str: string to print.
+ * Return: results.
+ */
+
+void puts2(char *str)
+{
+	int n = 0;
+	while (str[n] != '\0')
+	{
+		if (n % 2 == 0)
+			_putchar(str[n]);
+		n++;
+	}
+	_putchar('\n');
+}
